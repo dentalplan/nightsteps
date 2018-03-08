8dc379564c4b09bd0ffd74b962dd8dc8549d8bf0
dentalplan <clifford.hammett@gmail.com>
2018-03-08 11:25:17 +0000

    Adding gpio module

---
+++ ns_gpio.pm
@@ -0,0 +1,52 @@
+package ns_gpio{
+    use strict;
+    use warnings;
+    use Switch;
+
+    sub new{
+        my $class = shift;
+        my $this = {
+                    _mode => shift,
+                    _channel => shift,
+                    _datapath => '/home/pi/nsdata/gpio/',
+                    };
+        bless $this, $class;
+        $this->setupReading;
+        return $this;
+    }
+
+    sub setupReading{
+        my $this = shift;
+        system "touch $this->{_datapath}$this->{_channel}.$this->{_mode}";
+    }
+
+    sub readValue{
+        my $this = shift;
+        my $out;
+        switch ($this->{_mode}){
+            case 'a' {$out = $this->readAnalogue;}
+            case 'd' {$out = $this->readDigital;}
+#            case 'c' {$out = $this->readCompass;}
+        }
+        return $out;
+    }
+
+    sub readAnalogue{
+        my $this = shift;
+        open SENSOUT, "<$this->{_datapath}$this->{_channel}.$this->{_mode}" or die $!;
+        my @sens = <SENSOUT>;
+        my $size = @sens;
+        my $out = -1;
+        for(my $i=$size; $i>0 && $out > -1; $i--){
+            if ($sens[$i] =~ m/(\D\D\D\D)\n/){
+                $out = $1;
+            }
+        }   
+        return $out;
+    }
+
+    sub readDigital{
+        print "readDigital sub not yet written!!\n\n";
+    }
+}
+1;
