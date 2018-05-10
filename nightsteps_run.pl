0fe9b5fc050c484346d36722592d02f9e5fb03f4
dentalplan <clifford.hammett@gmail.com>
2018-05-10 09:20:57 +0100

    trying to add logic switch, plus slowing down gpio daemon

--- nightsteps_run.pl
+++ nightsteps_run.pl
@@ -1,12 +1,14 @@
 use strict;
 use warnings;
 use ns_loopit;
+use ns_gpio;
 
 #set up modules
 
 #set up variables
 my @listenshape = ([-6,-6], [-15,40], [15,40], [6,-6], [-6,-6]);
 my $maxdist = 45;
+my $switch1 = ns_gpio->new('a', 0);
 
 my $it = ns_loopit->new(    {    
                                 listenshape => \@listenshape,
@@ -17,6 +19,14 @@ my $it = ns_loopit->new(    {
 
 for (my $i=0;;$i++){
     $it->iterate;
+
+    my $read = $switch1->readValue;
+    if ($read > 1000){
+        $it->{logic} => "LRDBespeak1";
+    }else{ 
+        $it->{logic} => "LRDBchuck1";
+    }
+
     if ($i==1500){
         $i = 0;
 #        open(my $fh, "<", "/home/pi/nsdata/kill.switch");
