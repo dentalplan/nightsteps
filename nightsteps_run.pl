514bcf8b26217c3d20828577fc239c2c9391ccad
dentalplan <clifford.hammett@gmail.com>
2019-02-26 23:42:37 +0000

    added logic switch

--- nightsteps_run.pl
+++ nightsteps_run.pl
@@ -3,50 +3,50 @@ use warnings;
 use ns_loopit;
 use ns_gpio;
 
+
 #set up modules
 
 #set up variables
 my @listenshape = ([-6,-6], [-15,40], [15,40], [6,-6], [-6,-6]);
 my $maxdist = 45;
-my $switch1 = ns_gpio->new('a', 0);
+my $switch1 = ns_gpio->new('a', 7);
+my @switchbands = (
+    {low=>60, high=>95, logic=>'LRDBespeak1'},
+    {low=>135, high=>165, logic=>'LRDBchuck1'},
+    {low=>250, high=>280, logic=>''},
+    {low=>410, high=>450, logic=>''},
+    {low=>590, high=>635, logic=>''},
+    {low=>950, high=>1024, logic=>''}
+    );
+    
 
 my $it = ns_loopit->new(    {    
                                 listenshape => \@listenshape,
                                 logic => "dataLogger",
                                 maxdist => $maxdist,
                             });
-
+my $lastread = 0;
 for (my $i=0;;$i++){
 
-#    my $read = $switch1->readValue;
-#    print "SWITCH READING: $read\n";
-#    print "LOGIC: $it->{_logic}\n";
-#    if ($read > 900){
-#        print "over 900\n";
-#        if ($it->{_logic} ne "LRDBespeak1"){
-#            $it = ns_loopit->new(    {
-#                                      listenshape => \@listenshape,
-#                                      logic => "LRDBespeak1",
-#                                      maxdist => $maxdist,
-#                                    });
-#
-#        };
-#    }else{ 
-#        print "under 900\n";
-#        if ($it->{_logic} ne "LRDBchuck1"){
-#            $it = ns_loopit->new(    {
-#                                    listenshape => \@listenshape,
-#                                    logic => "LRDBchuck1",
-#                                    maxdist => $maxdist,
-#                                  });
-#
-#        }
-#    }
+    my $read = $switch1->readValue;
+    if ($read + 10 < $lastread || $read - 10 > $lastread){    print "SWITCH READING: $read\n"};
+    $lastread = $read;
+    foreach my $s (@switchbands){
+        if ($read > $s->{low} && $read < $s->{high} && $it->{_logic} ne $s->{logic}){
+            $it = ns_loopit->new(    {
+                                      listenshape => \@listenshape,
+                                      logic => $s->{logic},
+                                      maxdist => $maxdist,
+                                    });
+            print "switching to $s->{logic}\n";
+        }
+    }
 
     $it->iterate;
 
     if ($i==1500){
         $i = 0;
+#        print "SWITCH READING: $read\n";
 #        open(my $fh, "<", "/home/pi/nsdata/kill.switch");
     }
 }
