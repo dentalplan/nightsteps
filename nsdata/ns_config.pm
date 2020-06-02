34bd67348845bae0788bef2e87cbc994a3e60638
dentalplan <clifford.hammett@gmail.com>
2020-06-02 09:25:16 +0100

    adding base settings and query defs

--- nsdata/ns_config.pm
+++ nsdata/ns_config.pm
@@ -11,12 +11,11 @@ package ns_config{
     my $this = {};
     bless $this, $class;
     $this->{_json} = JSON->new;
-    $this = $this->defineControlParameters;
+    return $this;
   }
 
   sub defineControlParameters{
     my $this = shift;
-    my %database = (databaseName=>'ldd', databaseType=>'Pg');
     my $qs = $this->buildQuerySetup("/home/pi/nsdata/querydefs/ldd1.json");
     my @switchbands = (
         {low=>35, high=>110, logic=>'percussDemo'},
@@ -24,7 +23,7 @@ package ns_config{
         {low=>240, high=>350, logic=>'percussIt', query=>$qs, option=>"textsearch-demolition"},
         {low=>400, high=>500, logic=>'percussIt', query=>$qs, option=>"socialhousing-decrease"},
         {low=>580, high=>760, logic=>'percussIt', query=>$qs, option=>"socialhousing-increase"},
-        {low=>930, high=>1024, logic=>'percussIt', query=>$qs, option=>0}
+        {low=>930, high=>1024, logic=>'percussIt', query=>$qs, option=>"everything"}
         );
     my @dateScale = (
         {low=>0, high=>178, range=>'stillToCome'},
@@ -34,8 +33,8 @@ package ns_config{
     my %dateRangeProperties = (
             btmPin => 5,
             topPin => 6,
-            lowDate => DateTime->new(year=>2007, month=>8, day=>31),
-            highDate => DateTime->new(year=>2019, month=>9, day=>1),
+            lowDate => DateTime->new($qs->{minDate}),
+            highDate => DateTime->new($qs->{maxDate}),
             valScale => \@dateScale
         );
     $this->{_switchbands} = \@switchbands;
